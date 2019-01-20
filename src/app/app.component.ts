5780bf9b4eb3ef8eb3f4e77c9724ef8972bc038f
David Mora <davidmoracaceres20@gmail.com>
2019-01-20 18:55:05 +0100

    app CRUD sencillo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Producto } from './modelos/producto';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'CRUD-Sencillo-Angular';
+
+  constructor(){
+
+  }
+
+  texto: string = 'Insertar';
+
+  productoSeleccionado: Producto = new Producto();
+
+  listaProductos: Producto[] = [
+    {id:1, nombre: 'Pelota', precio: 15, categoria: 'Deporte'},
+    {id:2, nombre: 'Palo de golf', precio: 55, categoria: 'Deporte'},
+    {id:3, nombre: 'Sudadera', precio: 25, categoria: 'Ropa'}
+  ];
+
+
+  altaoEdita(){
+
+    if(this.productoSeleccionado.id == 0){
+      this.productoSeleccionado.id = this.listaProductos.length+1;
+      this.listaProductos.push(this.productoSeleccionado); 
+    }
+    this.productoSeleccionado = new Producto();
+    this.texto='Insertar';
+    
+  }
+
+  abrirEdicion(producto: Producto){
+    this.texto = 'Editar';
+    this.productoSeleccionado = producto;
+  }
+
+  eliminar(){
+    this.listaProductos = this.listaProductos.filter(elemento => elemento != this.productoSeleccionado);
+    this.productoSeleccionado = new Producto();
+  }
 }
